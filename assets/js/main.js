634833f5f00014a0fdcb75fdb8191f34f60e07b3
Dmytro <39014925+mustang-eleanor@users.noreply.github.com>
2018-07-06 15:01:40 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+var table = document.getElementsByTagName('table')[0];
+var cell = table.getElementsByTagName('td');
+var col, wh, lG;
+table.addEventListener('click', function(e) {
+  col = window.getComputedStyle(e.target, null).backgroundColor;
+  if (e.target.tagName === "TD") {
+    wh = 'rgba(0, 0, 0, 0)';
+    lG = 'rgb(144, 238, 144)';
+    if (col === wh) {
+      e.target.style.backgroundColor = lG;
+    } else {
+      e.target.style.backgroundColor = wh;
+    }
+  }
+}, false);
+    
+ 
+    
+    
+    
+    
+    
+
+ 
+
+
+
+
