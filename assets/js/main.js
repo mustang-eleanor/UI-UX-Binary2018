2a9cc5a1c7123752de707c999fdcf1311dcb5409
Dmytro <39014925+mustang-eleanor@users.noreply.github.com>
2018-07-07 10:35:29 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,23 @@
 let table = document.getElementById("table-time");
+let tableMobile = document.getElementById("table-time-mobile");
 let cell = table.getElementsByTagName('td');
 let col, wh, lG;
 
 
 table.addEventListener('click', function(e) {
+  col = window.getComputedStyle(e.target, null).backgroundColor;
+  if (e.target.tagName === "TD") {
+    wh = 'rgba(0, 0, 0, 0)';
+    lG = 'rgb(144, 238, 144)';
+    if (col === wh) {
+      e.target.style.backgroundColor = lG;
+    } else {
+      e.target.style.backgroundColor = wh;
+    }
+  }
+}, false);
+
+tableMobile.addEventListener('click', function(e) {
   col = window.getComputedStyle(e.target, null).backgroundColor;
   if (e.target.tagName === "TD") {
     wh = 'rgba(0, 0, 0, 0)';
